8673b4a98d0ca0b73d9c6f64b271c0e3a89d3542
sirin changulani <bappa123@sirins-MacBook-Air.local>
2025-01-08 00:45:14 +0530

    new

--- src/Confrence_selection.py
+++ src/Confrence_selection.py
@@ -50,15 +50,23 @@ def tmlr_suggestion(query):
   return completion.choices[0].message
 
 def final_conference():
-  query=''
-  score_cvpr= cvpr_suggestion(query)
-  score_tmlr= tmlr_suggestion(query)
-  score_kdd=  kdd_suggestion(query)
-  score_emnlp = emnlp_suggestion(query)
-  score_neurips= neurips_suggestion(query)
-  final_score = max(score_cvpr,score_emnlp,score_kdd,score_neurips,score_tmlr)
-  return final_score
-    
+    query = ''
+    score_cvpr = cvpr_suggestion(query)
+    score_tmlr = tmlr_suggestion(query)
+    score_kdd = kdd_suggestion(query)
+    score_emnlp = emnlp_suggestion(query)
+    score_neurips = neurips_suggestion(query)
+
+    scores = {
+        "cvpr": score_cvpr,
+        "tmlr": score_tmlr,
+        "kdd": score_kdd,
+        "emnlp": score_emnlp,
+        "neurips": score_neurips
+    }
+
+    final_conference = max(scores, key=scores.get)
+    return final_conference
 
 if __name__=='_main_':
   query=''
