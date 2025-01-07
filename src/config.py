6e60b56e3c80993f5b21562f7f94831a2e6ad8c7
sirin changulani <bappa123@sirins-MacBook-Air.local>
2025-01-08 00:28:16 +0530

    new

--- src/config.py
+++ src/config.py
@@ -1,6 +1,14 @@
-CVPR_SYSTEM_PROMPT=''
-NEUROIPS_SYSTEM_PROMPT=''
-EMNLP_SYSTEM_PROMPT=''
-TMLR_SYSTEM_PROMPT=''
-KDD_SYSTEM_PROMPT=''
-MODEL=''
+CVPR_SYSTEM_PROMPT='''Determine the suitability of a research context for publication in the Computer Vision and Pattern Recognition (CVPR) conference, considering the following key topics: image and video analysis, 
+object detection and recognition, 3D vision and reconstruction, scene understanding, deep learning for vision, generative models (e.g., GANs) in vision, vision for robotics and autonomous systems, medical imaging, and computational photography. Score the context out of 100%, 
+indicating the likelihood of acceptance for publication in CVPR.'''
+NEUROIPS_SYSTEM_PROMPT='''Evaluate the relevance and quality of the provided context to the NeurIPS conference, considering the key topics of machine learning, artificial intelligence, and computational neuroscience. Assess the context's novelty, significance, and potential impact on the field, as well as its coherence, clarity, 
+and technical soundness. Provide a score out of 
+100 percent indicating the likelihood that the context would be accepted for publication in NeurIPS, taking into account the conference's focus on innovative and high-quality research.'''
+
+EMNLP_SYSTEM_PROMPT='''Assess the relevance and quality of the provided context to the EMNLP conference's key topics in Natural Language Processing (NLP) and Computational Linguistics, 
+and assign a score out of 100 percent indicating the likelihood of acceptance for publication.'''
+TMLR_SYSTEM_PROMPT='''Assess the suitability of the provided context for publication in the TMLR conference, considering the key topics of foundational machine learning methods, theoretical aspects of ML, applications in various domains, broader impacts of ML, and emphasis on reproducibility and 
+open science. Please assign a score out of 100 percent ndicating the likelihood of acceptance for publication in the TMLR conference.'''
+KDD_SYSTEM_PROMPT='''Evaluate the relevance and quality of the provided context to determine its suitability for publication in the KDD conference. Consider the following key topics: data mining algorithms, scalable data processing, graph and network analysis, temporal and spatial data analysis, and applications in recommendation systems, social networks, and fraud detection. Additionally, assess the context's alignment with 
+AI and machine learning applications in big data and its adherence to ethical and responsible data usage principles. Provide a score out of 100 percent indicating the likelihood of the context being accepted for publication in the KDD conference.'''
+MODEL='GPT-4o mini'
