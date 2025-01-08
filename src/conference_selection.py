0ab3206e7e82edebfd658005eb4d4d683d68a51f
NisargUpadhyayIITJ <141734373+NisargUpadhyayIITJ@users.noreply.github.com>
2025-01-08 21:35:24 +0530

    Structured output with parallel processing

--- src/conference_selection.py
+++ src/conference_selection.py
@@ -2,88 +2,211 @@ from openai import OpenAI
 from concurrent.futures import ThreadPoolExecutor
 from config import *
 from loguru import logger
+import json
+import re
 
 client = OpenAI()
 
-def cvpr_suggestion(research_paper: str) -> str:
-  completion = client.chat.completions.create(
-  model=MODEL,
-  messages=[
-    {"role": "system", "content": CVPR_SYSTEM_PROMPT},
-    {"role": "user", "content": research_paper}
-  ]
-  )
-  return completion.choices[0].message.content
+def parse_tool_call_str(tool_call_str: str):
+    pattern = r'</?tool_call>'
+    clean_tags = re.sub(pattern, '', tool_call_str)
+    
+    try:
+        tool_call_json = json.loads(clean_tags)
+        return tool_call_json
+    except json.JSONDecodeError:
+        return clean_tags
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return "There was some error parsing the Tool's output"
+
+def cvpr_suggestion(research_paper: str) -> dict:
+    tool_chat_history = [
+        {
+            "role": "system",
+            "content": TOOL_SYSTEM_PROMPT_CVPR
+        }
+    ]
+
+    user_msg = {
+        "role": "user",
+        "content": research_paper
+    }
+
+    tool_chat_history.append(user_msg)
+
+    output = client.chat.completions.create(
+        messages=tool_chat_history,
+        model=MODEL
+    ).choices[0].message.content
+
+    # Extract tool call strings using regular expressions
+    tool_call_pattern = r'<tool_call>(.*?)</tool_call>'
+    tool_calls = re.findall(tool_call_pattern, output, re.DOTALL)
+
+    # Loop over each tool call and extract arguments
+    tool_call_json = parse_tool_call_str(tool_calls[0])  # Parse the cleaned tool call string
+    if isinstance(tool_call_json, dict) and "arguments" in tool_call_json:
+        args = tool_call_json["arguments"]
+    return args      
 
 def neurips_suggestion(research_paper: str) -> str:
-  completion = client.chat.completions.create(
-  model=MODEL,
-  messages=[
-    {"role": "system", "content": NS_SYSTEM_PROMPT},
-    {"role": "user", "content": research_paper}
-  ]
-  )
-  return completion.choices[0].message.content
+    tool_chat_history = [
+        {
+            "role": "system",
+            "content": TOOL_SYSTEM_PROMPT_NEUR_IPS
+        }
+    ]
+
+    user_msg = {
+        "role": "user",
+        "content": research_paper
+    }
+
+    tool_chat_history.append(user_msg)
+
+    output = client.chat.completions.create(
+        messages=tool_chat_history,
+        model=MODEL
+    ).choices[0].message.content
+
+    # Extract tool call strings using regular expressions
+    tool_call_pattern = r'<tool_call>(.*?)</tool_call>'
+    tool_calls = re.findall(tool_call_pattern, output, re.DOTALL)
+
+    # Loop over each tool call and extract arguments
+    tool_call_json = parse_tool_call_str(tool_calls[0])  # Parse the cleaned tool call string
+    if isinstance(tool_call_json, dict) and "arguments" in tool_call_json:
+        args = tool_call_json["arguments"]
+    return args 
 
 def emnlp_suggestion(research_paper: str) -> str:
-  completion = client.chat.completions.create(
-  model=MODEL,
-  messages=[
-    {"role": "system", "content": EMNLP_SYSTEM_PROMPT},
-    {"role": "user", "content": research_paper}
-  ]
-  )
-  return completion.choices[0].message.content
+    tool_chat_history = [
+        {
+            "role": "system",
+            "content": TOOL_SYSTEM_PROMPT_EMNLP
+        }
+    ]
+
+    user_msg = {
+        "role": "user",
+        "content": research_paper
+    }
+
+    tool_chat_history.append(user_msg)
+
+    output = client.chat.completions.create(
+        messages=tool_chat_history,
+        model=MODEL
+    ).choices[0].message.content
+
+    # Extract tool call strings using regular expressions
+    tool_call_pattern = r'<tool_call>(.*?)</tool_call>'
+    tool_calls = re.findall(tool_call_pattern, output, re.DOTALL)
+
+    # Loop over each tool call and extract arguments
+    tool_call_json = parse_tool_call_str(tool_calls[0])  # Parse the cleaned tool call string
+    if isinstance(tool_call_json, dict) and "arguments" in tool_call_json:
+        args = tool_call_json["arguments"]
+    return args 
 
 def kdd_suggestion(research_paper: str) -> str:
-  completion = client.chat.completions.create(
-  model=MODEL,
-  messages=[
-    {"role": "system", "content": KDD_SYSTEM_PROMPT},
-    {"role": "user", "content": research_paper}
-  ]
-  )
-  return completion.choices[0].message.content
+    tool_chat_history = [
+        {
+            "role": "system",
+            "content": TOOL_SYSTEM_PROMPT_KDD
+        }
+    ]
+
+    user_msg = {
+        "role": "user",
+        "content": research_paper
+    }
+
+    tool_chat_history.append(user_msg)
+
+    output = client.chat.completions.create(
+        messages=tool_chat_history,
+        model=MODEL
+    ).choices[0].message.content
+
+    # Extract tool call strings using regular expressions
+    tool_call_pattern = r'<tool_call>(.*?)</tool_call>'
+    tool_calls = re.findall(tool_call_pattern, output, re.DOTALL)
+
+    # Loop over each tool call and extract arguments
+    tool_call_json = parse_tool_call_str(tool_calls[0])  # Parse the cleaned tool call string
+    if isinstance(tool_call_json, dict) and "arguments" in tool_call_json:
+        args = tool_call_json["arguments"]
+    return args 
 
 def tmlr_suggestion(research_paper: str) -> str:
-  completion = client.chat.completions.create(
-  model=MODEL,
-  messages=[
-    {"role": "system", "content": TMLR_SYSTEM_PROMPT},
-    {"role": "user", "content": research_paper}
-  ]
-  )
-  return completion.choices[0].message.content
+    tool_chat_history = [
+        {
+            "role": "system",
+            "content": TOOL_SYSTEM_PROMPT_TMLR
+        }
+    ]
+
+    user_msg = {
+        "role": "user",
+        "content": research_paper
+    }
+
+    tool_chat_history.append(user_msg)
+
+    output = client.chat.completions.create(
+        messages=tool_chat_history,
+        model=MODEL
+    ).choices[0].message.content
+
+    # Extract tool call strings using regular expressions
+    tool_call_pattern = r'<tool_call>(.*?)</tool_call>'
+    tool_calls = re.findall(tool_call_pattern, output, re.DOTALL)
+
+    # Loop over each tool call and extract arguments
+    tool_call_json = parse_tool_call_str(tool_calls[0])  # Parse the cleaned tool call string
+    if isinstance(tool_call_json, dict) and "arguments" in tool_call_json:
+        args = tool_call_json["arguments"]
+    return args 
 
 def final_conference(research_paper: str) -> str:
-  with ThreadPoolExecutor() as executor:
-    future_cvpr = executor.submit(cvpr_suggestion, research_paper)
-    future_tmlr = executor.submit(cvpr_suggestion, research_paper)
-    future_kdd = executor.submit(cvpr_suggestion, research_paper)
-    future_emnlp = executor.submit(cvpr_suggestion, research_paper)
-    future_neurips = executor.submit(cvpr_suggestion, research_paper)
+    with ThreadPoolExecutor() as executor:
+      future_cvpr = executor.submit(cvpr_suggestion, research_paper)
+      future_tmlr = executor.submit(cvpr_suggestion, research_paper)
+      future_kdd = executor.submit(cvpr_suggestion, research_paper)
+      future_emnlp = executor.submit(cvpr_suggestion, research_paper)
+      future_neurips = executor.submit(cvpr_suggestion, research_paper)
 
-    score_cvpr = future_cvpr.result()
-    score_tmlr = future_tmlr.result()
-    score_kdd = future_kdd.result()
-    score_emnlp = future_emnlp.result()
-    score_neurips = future_neurips.result()
+      dict_cvpr = future_cvpr.result()
+      dict_tmlr = future_tmlr.result()
+      dict_kdd = future_kdd.result()
+      dict_emnlp = future_emnlp.result()
+      dict_neurips = future_neurips.result()
 
-  scores = {
-      "cvpr": score_cvpr,
-      "tmlr": score_tmlr,
-      "kdd": score_kdd,
-      "emnlp": score_emnlp,
-      "neurips": score_neurips
-  }
+    logger.info(f"dict_cvpr: {dict_cvpr}") 
+    logger.info(f"Reason: {dict_cvpr['reason']}") 
 
-  logger.info(f"Scores: {scores}")
+    # Consolidating scores into a dictionary with their names
+    all_dicts = {
+        "cvpr": dict_cvpr,
+        "tmlr": dict_tmlr,
+        "kdd": dict_kdd,
+        "emnlp": dict_emnlp,
+        "neurips": dict_neurips
+    }
 
-  final_conference = max(scores, key=scores.get)
-  return final_conference
+    # Finding the dict with the maximum score
+    max_name, max_dict = max(all_dicts.items(), key=lambda x: x[1]["score"])
+
+    # Displaying the name and reason in the desired format
+    output = [max_name, max_dict["reason"]]
+    print(output)
 
 if __name__=='_main_':
   research_paper=''
   final_conference(research_paper)
+
+
   
 
--- src/config.py
+++ src/config.py
@@ -1,19 +1,221 @@
-CVPR_SYSTEM_PROMPT='''Determine the suitability of a research context for publication in the Computer Vision and Pattern Recognition (CVPR) conference, considering the following key topics: image and video analysis, 
-object detection and recognition, 3D vision and reconstruction, scene understanding, deep learning for vision, generative models (e.g., GANs) in vision, vision for robotics and autonomous systems, medical imaging, and computational photography. Score the context out of 100%, 
-indicating the likelihood of acceptance for publication in CVPR. Strictly provide a score between 0 and 100 only. Nothing else.'''
+CVPR_SYSTEM_PROMPT='''Determine the suitability of a research context for publication in the Computer Vision
+and Pattern Recognition (CVPR) conference, considering the following key topics: image and video analysis, 
+object detection and recognition, 3D vision and reconstruction, scene understanding, deep learning for
+vision, generative models (e.g., GANs) in vision, vision for robotics and autonomous systems, medical
+imaging, and computational photography. Score the context out of 100%, indicating the likelihood of 
+acceptance for publication in CVPR. Strictly provide a score from 0 to 100 and the reasoning behind 
+why the paper should get accepted in the conference. The format of the output shoud 
+be a dictionary of the format {'score': score, 'reason': reason}. Strictly follow the above format.'''
 
-NS_SYSTEM_PROMPT='''Evaluate the relevance and quality of the provided context to the NeurIPS conference, considering the key topics of machine learning, artificial intelligence, and computational neuroscience. Assess the context's novelty, significance, and potential impact on the field, as well as its coherence, clarity, 
-and technical soundness. Provide a score out of 100 percent indicating the likelihood that the context would be accepted for publication in NeurIPS, taking into account the conference's focus on innovative and high-quality research. Strictly provide a score between 0 and 100 only. Nothing else.'''
+NS_SYSTEM_PROMPT='''Evaluate the relevance and quality of the provided context to the NeurIPS conference, 
+considering the key topics of machine learning, artificial intelligence, and computational neuroscience. 
+Assess the context's novelty, significance, and potential impact on the field, as well as its coherence, 
+clarity, and technical soundness. Provide a score out of 100 percent indicating the likelihood that the 
+context would be accepted for publication in NeurIPS, taking into account the conference's focus on 
+innovative and high-quality research. Strictly provide a score from 0 to 100 and the reasoning behing 
+why the paper should get accepted in the conference. The format of the output shoud be a dictionary of 
+the format {'score': score, 'reason': reason}. Strictly follow the above format.'''
 
-EMNLP_SYSTEM_PROMPT='''Assess the relevance and quality of the provided context to the EMNLP conference's key topics in Natural Language Processing (NLP) and Computational Linguistics, 
-and assign a score out of 100 percent indicating the likelihood of acceptance for publication.'''
+EMNLP_SYSTEM_PROMPT='''Assess the relevance and quality of the provided context to the EMNLP conference's 
+key topics in Natural Language Processing (NLP) and Computational Linguistics, and assign a score out of 
+100 percent indicating the likelihood of acceptance for publication. Strictly provide a score from 0 to 100 
+and the reasoning behing why the paper should get accepted in the conference. The format of the output 
+shoud be a dictionary of the format {'score': score, 'reason': reason}. Strictly follow the above format.'''
 
-TMLR_SYSTEM_PROMPT='''Assess the suitability of the provided context for publication in the TMLR conference, considering the key topics of foundational machine learning methods, theoretical aspects of ML, applications in various domains, broader impacts of ML, and emphasis on reproducibility and 
-open science. Please assign a score out of 100 percent ndicating the likelihood of acceptance for publication in the TMLR conference. Strictly provide a score between 0 and 100 only. Nothing else.'''
+TMLR_SYSTEM_PROMPT='''Assess the suitability of the provided context for publication in the TMLR conference, 
+considering the key topics of foundational machine learning methods, theoretical aspects of ML, 
+applications in various domains, broader impacts of ML, and emphasis on reproducibility and open science. 
+Assign a score out of 100 percent indicating the likelihood of acceptance for publication in the TMLR 
+conference. Strictly provide a score from 0 to 100 and the reasoning behing why the paper should get 
+accepted in the conference. The format of the output shoud be a dictionary of the format {'score': score, 
+'reason': reason}. Strictly follow the above format.'''
 
-KDD_SYSTEM_PROMPT='''Evaluate the relevance and quality of the provided context to determine its suitability for publication in the KDD conference. Consider the following key topics: data mining algorithms, scalable data processing, graph and network analysis, temporal and spatial data analysis, and applications in recommendation systems, social networks, and fraud detection. Additionally, assess the context's alignment with 
-AI and machine learning applications in big data and its adherence to ethical and responsible data usage principles. Provide a score out of 100 percent indicating the likelihood of the context being accepted for publication in the KDD conference.  Strictly provide a score between 0 and 100 only. Nothing else.'''
+KDD_SYSTEM_PROMPT='''Evaluate the relevance and quality of the provided context to determine its 
+suitability for publication in the KDD conference. Consider the following key topics: data mining 
+algorithms, scalable data processing, graph and network analysis, temporal and spatial data analysis, 
+and applications in recommendation systems, social networks, and fraud detection. Additionally, assess 
+the context's alignment with AI and machine learning applications in big data and its adherence to ethical 
+and responsible data usage principles. Provide a score out of 100 percent indicating the likelihood of the 
+context being accepted for publication in the KDD conference. Strictly provide a score from 0 to 100 and 
+the reasoning behing why the paper should get accepted in the conference. The format of the output shoud 
+be a dictionary of the format {'score': score, 'reason': reason}. Strictly follow the above format.'''
 
-EVALUATOR_PROMPT='''You are an expert in distinguishing between publishable and non-publishable research paper by comparing the given research paper with a set of benchmark papers. Please evaluate the following context based on its relevance, significance, technical soundness, and potential impact on the field. Output "Yes" if the context is suitable for publication and "No" if it is not. Strictly follow the given instructions.'''
+EVALUATOR_PROMPT='''You are an expert in distinguishing between publishable and non-publishable research 
+paper. Compare the given research paper with a set of benchmark papers. Please evaluate the following 
+context based on its relevance, significance, technical soundness, and potential impact on the field.
+Output "Yes" if the context is suitable for publication and "No" if it is not. Strictly follow the given 
+instructions.'''
 
 MODEL='gpt-4o-mini'
+
+TOOL_SYSTEM_PROMPT_CVPR='''
+You are an evaluation AI model. You are provided with function signatures within <tools></tools> XML tags. 
+You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug 
+into functions. Pay special attention to the properties 'types'. You should use those types as in a Python dict.
+For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
+
+<tool_call>
+{"name": <function-name>,"arguments": <args-dict>}
+</tool_call>
+
+Here are the available tools:
+
+<tools> {
+    "name": "evaluate_cvpr_paper",
+    "description": "Determine the suitability of a research context for publication in the Computer Vision
+                    and Pattern Recognition (CVPR) conference, considering the following key topics: image and video analysis, 
+                    object detection and recognition, 3D vision and reconstruction, scene understanding, deep learning for
+                    vision, generative models (e.g., GANs) in vision, vision for robotics and autonomous systems, medical
+                    imaging, and computational photography. Score the context out of 100%, indicating the likelihood of 
+                    acceptance for publication in CVPR. score (str): Score given to the research paper 
+                    reason (str): Reason for the paper being selected in the conference",
+    "parameters": {
+        "properties": {
+            "score": {
+                "type": "str"
+            },
+            "reason": {
+                "type": "str"
+            }
+        }
+    }
+}
+</tools>
+'''
+
+TOOL_SYSTEM_PROMPT_NEUR_IPS='''
+You are an evaluation AI model. You are provided with function signatures within <tools></tools> XML tags. 
+You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug 
+into functions. Pay special attention to the properties 'types'. You should use those types as in a Python dict.
+For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
+
+<tool_call>
+{"name": <function-name>,"arguments": <args-dict>}
+</tool_call>
+
+Here are the available tools:
+
+<tools> {
+    "name": "evaluate_neur_ips_paper",
+    "description": "Evaluate the relevance and quality of the provided context to the NeurIPS conference, 
+                    considering the key topics of machine learning, artificial intelligence, and computational neuroscience. 
+                    Assess the context's novelty, significance, and potential impact on the field, as well as its coherence, 
+                    larity, and technical soundness. Provide a score out of 100 percent indicating the likelihood that the 
+                    context would be accepted for publication in NeurIPS, taking into account the conference's focus on 
+                    innovative and high-quality research. score (str): Score given to the research paper 
+                    reason (str): Reason for the paper being selected in the conference",
+    "parameters": {
+        "properties": {
+            "score": {
+                "type": "str"
+            },
+            "reason": {
+                "type": "str"
+            }
+        }
+    }
+}
+</tools>
+'''
+
+TOOL_SYSTEM_PROMPT_EMNLP='''
+You are an evaluation AI model. You are provided with function signatures within <tools></tools> XML tags. 
+You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug 
+into functions. Pay special attention to the properties 'types'. You should use those types as in a Python dict.
+For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
+
+<tool_call>
+{"name": <function-name>,"arguments": <args-dict>}
+</tool_call>
+
+Here are the available tools:
+
+<tools> {
+    "name": "evaluate_emnlp_paper",
+    "description": "Assess the relevance and quality of the provided context to the EMNLP conference's 
+                    key topics in Natural Language Processing (NLP) and Computational Linguistics, and assign a score out of 
+                    100 percent indicating the likelihood of acceptance for publication. score (str): Score given to the research paper 
+                    reason (str): Reason for the paper being selected in the conference",
+    "parameters": {
+        "properties": {
+            "score": {
+                "type": "str"
+            },
+            "reason": {
+                "type": "str"
+            }
+        }
+    }
+}
+</tools>
+'''
+
+TOOL_SYSTEM_PROMPT_TMLR='''
+You are an evaluation AI model. You are provided with function signatures within <tools></tools> XML tags. 
+You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug 
+into functions. Pay special attention to the properties 'types'. You should use those types as in a Python dict.
+For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
+
+<tool_call>
+{"name": <function-name>,"arguments": <args-dict>}
+</tool_call>
+
+Here are the available tools:
+
+<tools> {
+    "name": "evaluate_tmlr_paper",
+    "description": "Assess the suitability of the provided context for publication in the TMLR conference, 
+                    considering the key topics of foundational machine learning methods, theoretical aspects of ML, 
+                    applications in various domains, broader impacts of ML, and emphasis on reproducibility and open science. 
+                    Assign a score out of 100 percent indicating the likelihood of acceptance for publication in the TMLR 
+                    conference. score (str): Score given to the research paper 
+                    reason (str): Reason for the paper being selected in the conference",
+    "parameters": {
+        "properties": {
+            "score": {
+                "type": "str"
+            },
+            "reason": {
+                "type": "str"
+            }
+        }
+    }
+}
+</tools>
+'''
+
+TOOL_SYSTEM_PROMPT_KDD='''
+You are an evaluation AI model. You are provided with function signatures within <tools></tools> XML tags. 
+You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug 
+into functions. Pay special attention to the properties 'types'. You should use those types as in a Python dict.
+For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
+
+<tool_call>
+{"name": <function-name>,"arguments": <args-dict>}
+</tool_call>
+
+Here are the available tools:
+
+<tools> {
+    "name": "evaluate_kdd_paper",
+    "description": "Evaluate the relevance and quality of the provided context to determine its 
+                    suitability for publication in the KDD conference. Consider the following key topics: data mining 
+                    algorithms, scalable data processing, graph and network analysis, temporal and spatial data analysis, 
+                    and applications in recommendation systems, social networks, and fraud detection. Additionally, assess 
+                    the context's alignment with AI and machine learning applications in big data and its adherence to ethical 
+                    and responsible data usage principles. Provide a score out of 100 percent indicating the likelihood of the 
+                    context being accepted for publication in the KDD conference. score (str): Score given to the research paper 
+                    reason (str): Reason for the paper being selected in the conference",
+    "parameters": {
+        "properties": {
+            "score": {
+                "type": "str"
+            },
+            "reason": {
+                "type": "str"
+            }
+        }
+    }
+}
+</tools>
+'''
